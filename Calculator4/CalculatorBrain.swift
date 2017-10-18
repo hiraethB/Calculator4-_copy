247e76c4840e9d93a57fa3f9c43445da421ec0a2
Boris V <bforeknown@gmail.com>
2017-10-18 16:21:15 +0300

    After Assingment1

--- Calculator4/CalculatorBrain.swift
+++ Calculator4/CalculatorBrain.swift
@@ -2,41 +2,46 @@
 //  CalculatorBrain.swift
 //  Calculator4
 //
-//  Created by Борис Винник on 25.09.17.
+//  Created by Boris V on 25.09.17.
 //  Copyright © 2017 GRIAL. All rights reserved.
 //
 
-
 import Foundation
-
-private struct PendingBinaryOperation {
+private struct PendingBinaryOperation { // структура для запоминания свойств отложенной бинарной операции
     let function: (Double, Double) -> Double
-    let firstOperand: Double
-    
+    let firstOperand: Double // сохранение первого оператора бинарной функции
+    var description: String
     func perform(with secondOperand: Double) -> Double {
         return function(firstOperand, secondOperand)
     }
-    
 }
-
+let numberFormatter: NumberFormatter = { // Форматтер, который преобразует числовые значения и их текстовые представления, свойство класса NumberFormatter
+    let formatter = NumberFormatter () // создаем методы
+    formatter.numberStyle = .decimal
+    formatter.maximumFractionDigits = 6
+    formatter.locale = NSLocale.current
+    return formatter
+} ()
 struct CalculatorBrain {
-    
-    private var accumulator: Double?
-    
-    
-    private enum Operation {
+    var accumulator: ( value: Double?, description: String) = ( nil, "")
+    mutating func initialization () {
+        setOperand(0.0)
+        pendingBinaryOperation = nil
+        resultIsPending = false
+    }
+    enum Operation {
         case constant(Double)
         case unaryOperation(( Double) -> Double)
         case binaryOperation ((Double, Double) -> Double)
         case equals
-        
+        case randOperation(() -> Double)
     }
-    
     private var operations: Dictionary<String,Operation> = [
         "π" : Operation.constant(Double.pi),
         "e" : Operation.constant(M_E),
         "√" : Operation.unaryOperation(sqrt),
-        "sin" : Operation.unaryOperation(_sin),
+        "∛" : Operation.unaryOperation{pow($0, 1.0/3)},
+        "sin" : Operation.unaryOperation(sin),
         "cos" : Operation.unaryOperation(cos),
         "±" : Operation.unaryOperation{ -$0 },
         "×" : Operation.binaryOperation{ $0 * $1 },
@@ -44,44 +49,56 @@ struct CalculatorBrain {
         "+" : Operation.binaryOperation{ $0 + $1 },
         "-" : Operation.binaryOperation{ $0 - $1 },
         "x²" : Operation.unaryOperation{ $0*$0 },
-        "x³" : Operation.unaryOperation{ $0*$0*$0 },
+        "x³" : Operation.unaryOperation{ pow($0,3) },
         "=" : Operation.equals,
-       // "Rad"   // Переключение между градусами (Deg) и радианами (Rad)     
+        "Rand" : Operation.randOperation{ Double(arc4random()) / Double(UInt32.max) } // Генерирует случайное число с плавающей точкой от 0 до 1 двойной точности
     ]
+    var resultIsPending = false //флаг отложенной бинарной операции
+    private var pendingBinaryOperation: PendingBinaryOperation? // отложенная бинарная операция
     mutating func performOperation(_ symbol: String) {
         if let operation = operations[symbol] {
             switch operation {
-            case .constant(let value):
-                accumulator = value
-            case .unaryOperation(let function):
-                if accumulator != nil {
-                    accumulator = function(accumulator!)
-                }
             case .binaryOperation(let function):
-                if accumulator != nil {
-                    pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!)
-                    accumulator = nil
+                performPendingBinaryOperation() // согласно (6х5х4х3=) hw.hint 1.7
+                if accumulator.value != nil { // есть значение (запомнить первый операнд и операцию с описанием их последовательности)
+                    pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator.value!, description: accumulator.description + symbol)
+                    if (symbol == "×" || symbol == "÷") {
+                        pendingBinaryOperation!.description = ("(" + accumulator.description + ")" + symbol) // добавить "(" и ")" в описание последовательности для "×" или "÷"
+                    }
+                accumulator = (nil, pendingBinaryOperation!.description)
+                resultIsPending = true
                 }
+            case .constant(let value):
+                accumulator = (value, symbol)
             case .equals:
-                performPendingBinaryOperation()
+                performPendingBinaryOperation() // к выполнению бинарной операции
+            case .unaryOperation(let function):
+                var relation: String
+                if accumulator.value != nil {
+                    if resultIsPending  {
+                        relation = pendingBinaryOperation!.description + symbol + "(" + accumulator.description + ")"
+                        pendingBinaryOperation!.description = String() // сбросить описание последовательности в буфере отложенной бинарной операции
+                    } else { // нет отложенной бинарной операции
+                        relation = symbol + "(" + accumulator.description + ")"
+                    }
+                    accumulator = (function(accumulator.value!), relation)
+                }
+            case .randOperation (let function):
+                accumulator = (function(), symbol)
             }
         }
     }
     private mutating func performPendingBinaryOperation() {
-        if pendingBinaryOperation != nil && accumulator != nil {
-            accumulator = pendingBinaryOperation!.perform(with: accumulator!)
+        if pendingBinaryOperation != nil && accumulator.value != nil {
+            accumulator = (pendingBinaryOperation!.perform( with: accumulator.value!), pendingBinaryOperation!.description + accumulator.description )
             pendingBinaryOperation = nil
-        
+            resultIsPending = false
         }
     }
-    private var pendingBinaryOperation: PendingBinaryOperation?
-    mutating func setOperand(_ operand: Double) {
-        accumulator = operand
-    }
-    
-    var result: Double? {
-        get {
-            return accumulator
+    mutating func setOperand(_ operand: Double) { // передача операнда из ViewController во время выполнения операции
+        accumulator.value = operand
+        if let value = accumulator.value { // есть операнд?
+            accumulator.description = numberFormatter.string(from: NSNumber(value: value)) ?? "..."
         }
     }
 }
--- Calculator4/ViewController.swift
+++ Calculator4/ViewController.swift
@@ -2,49 +2,71 @@
 //  ViewController.swift
 //  Calculator4
 //
-//  Created by Борис Винник on 25.09.17.
+//  Created by Boris V on 25.09.17.
 //  Copyright © 2017 GRIAL. All rights reserved.
 //
 
 import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
-    var userInTheMiddleOfTyping = false
+    @IBOutlet weak var sequence: UILabel!
+    private var userInTheMiddleOfTyping = false // флаг начатого и незаконченного цифрового ввода
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
-        if userInTheMiddleOfTyping { // это не первый символ?
-            let textCurrentlyInDisplay = display.text! // не первый
+        if userInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
             if !(textCurrentlyInDisplay.contains (".")) || (digit != ".") {
                 display.text = textCurrentlyInDisplay + digit
             }
-        }
-        else { // это первый вводимый символ с цифровой клавиатуры
+        } else { // это первый вводимый символ с цифровой клавиатуры
             display.text = digit
-           // if digit != "." {
-                userInTheMiddleOfTyping = true
-           // }
+            userInTheMiddleOfTyping = true
+        }
+        if !brain.resultIsPending { // пример 5+6=7 будет показано “… “ ( 7 на display)
+            sequence.text = "..."
         }
     }
-    var displayValue : Double { // вычисляемое свойство
+    private var displayValue : Double? { // разрешение предположения профессора (всегда ли строку цифрового ввода можно интерпретировать как Double)
         get {
-            return Double ( display.text!)! // (вычислить) преобразовать получаемое значение в Double //???? случай ввода(.π.) или (π.π)
-            // ошибка происходит из-за предположения, что строку на дисплее всегда можно интерпретировать как Double.
-        }
-        set {
-            display.text = String( newValue)
+            if display.text != nil { // введено число ?
+                return Double (display.text!) // вернуть Double
+            }
+                return nil // не число, вернуть опционалу "not set"
+        } set {
+            if let new = newValue {
+                display.text = numberFormatter.string(from: NSNumber( value: new))
+            }
         }
     }
     private var brain = CalculatorBrain()
-    @IBAction func performOperation(_ sender: UIButton) {
-        if userInTheMiddleOfTyping { // Если в середине ввода, то при вводе операции
-            brain.setOperand(displayValue) //  установить operand
-            userInTheMiddleOfTyping = false // сбросить флаг "середина ввода" в false
+    @IBAction func performOperation(_ sender: UIButton) { // выполнить операцию
+        if userInTheMiddleOfTyping { // Если в середине ввода числа, то при вводе операции
+            if displayValue != nil {
+                brain.setOperand( displayValue!) //  установить операнд и описание операции
+            }
+            userInTheMiddleOfTyping = false // зафиксировать окончание ввода операнда
         }
-        if let mathematicalSymbol = sender.currentTitle { // выполнение операции в начале ввода
-            brain.performOperation(mathematicalSymbol) // передача символа операции для вычисления
+        if let mathematicalSymbol = sender.currentTitle { // символ операции определён
+            brain.performOperation(mathematicalSymbol) // передать для вычисления в Модель
         }
-        if let result = brain.result { // получение результата операции
-            displayValue = result
+        displayValue = brain.accumulator.value // получение результата вычислений
+        sequence.text = brain.accumulator.description + (brain.resultIsPending ? "..." : "=")
+    }
+    @IBAction func initialization(_ sender: UIButton) {
+        display.text = "0" // начальное значение дисплея
+        userInTheMiddleOfTyping = false
+        sequence.text = String() // пустая строка ленты
+        brain.initialization()
+    }
+    @IBAction func backSpace(_ sender: UIButton) {
+        if display.text != nil && userInTheMiddleOfTyping {
+            if display.text!.characters.count > 1 { // количество символов строки дисплея
+                display.text!.characters.removeLast() // стирает крайний правый, последний из введенных, символ цифры или точки
+            } else {
+                display.text = "0"
+                userInTheMiddleOfTyping = false
+            }
         }
     }
 }
+
